1e70483d17fb01ce585081bb7df52cfeabe4820b
Anvin George <anvingeorge22@gmail.com>
2024-07-21 18:57:41 +0530

    first commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,45 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns Android?",
+            "Which one is not a programming language?",
+            "Who is our principal?",
+            "Which company owns vision pro?",
+            "How many students are in S4 CS1?",
+            "Which is the capital of France?",
+            "What is the largest mammal?",
+            "Who wrote 'Romeo and Juliet'?",
+            "Which planet is known as the Red Planet?",
+            "What is the currency of Japan?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Neethu Krishna","Anitha G Pillai","Aishwarya Menon","Sophia Cleetus"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"65","68","75","71"},
+            {"Paris","London","Berlin","Rome"},
+            {"Elephant","Blue Whale","Giraffe","African Elephant"},
+            {"William Shakespeare","Jane Austen","Charles Dickens","Mark Twain"},
+            {"Mars","Venus","Mercury","Jupiter"},
+            {"Dollar","Euro","Yen","Pound"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Anitha G Pillai",
+            "Apple",
+            "71",
+            "Paris",
+            "Blue Whale",
+            "William Shakespeare",
+            "Mars",
+            "Yen"
+    };
+
+
+}
